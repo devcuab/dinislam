4224aea1bd30073dc6fde654a497a72b8891ae9a
dinislam <dinislam.fevcuab@gmail.com>
2025-08-14 11:23:14 +0500

    тоже недоработка

--- asd.py
+++ asd.py
@@ -2,16 +2,41 @@
 
 data_of_music = {
     'songs' : [
-        {"tittle": "asd", "artist": "me", "genre": "stress"},
-        {"tittle": "haha", "artist": "not me", "genre": "hipe"}        
+        {"tittle": "asd", "artist": "me"},
+        {"tittle": "haha", "artist": "not me"}        
     ]
     }
 
 def GetValue(data: list, type: str, choice: str):
     if data:
-        for dat in data['songs']:
-            if type is "tittle":
+        if type == "tittle":
+            result = next(filter(lambda da: da["tittle"] == choice, data['songs']))
+            if result:
+                return_data = ""
+                for res in result:
+                    return_data += f"песня: (титул:{res["tittle"]}, исполнитель:{res["artist"]}) \n"
+                return return_data
+            else:
+                return "Песни не были найдены"
+        
                 
+        if type == "artist":
+            result = next(filter(lambda da: da["artist"] == choice, data["songs"]))
+            if result:
+                return_data = ""
+                for res in result:
+                    return_data += f"песня: (титул:{res["tittle"]}, исполнитель:{res["artist"]}) \n"
+                return return_data
+            else:
+                return "Песни не были найдены"
     else:
         return "Ошибка данные пустые"    
     
+
+if __name__ == "__main__":
+    while True:
+        choised_type = input("Введите по какому критерию будет поиск: (исполнитель \ титул)")
+        if choised_type != "исполнитель" or choised_type != "титул":
+            print("Ошибка: не корректный выбор")
+        choised_ch = input("Введите поиск по выбранному критерию")
+        print(GetValue(data=data_of_music, type=choised_type, choice=choised_ch))
